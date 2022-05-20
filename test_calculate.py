68b0bbcfaf5fe56c9ef933b86b2855ed57ce3633
dborodin836 <dborodin836@gmail.com>
2022-05-20 14:07:35 +0300

    tests

--- test_calculate.py
+++ test_calculate.py
@@ -1,26 +1,17 @@
-def translate_to_python(row: str) -> str:
-    if '(' not in row:
-        return replace_all(row)
-    while '(' in row:
-        if row.count('(') == 1:
-            start = row.index('(')
-            end = row.rindex(')')
-        else:
-            end = row.index(')')
-            start = row.rindex('(', 0, end)
-        replaced = translate_to_python(row[start + 1: end])
-        row = row[:start] + replaced + row[end + 1:]
-    return row
+from main import translate_to_python
 
 
-def replace_all(row: str) -> str:
-    if '⊕' in row:
-        index = row.index('⊕')
-        no_of_negative = row.count('̅')
-        if no_of_negative == 0:
-            return f"xor[{row[index - 1]}, {row[index + 1]}]"
-    return row
+def test_basic():
+    assert translate_to_python('(X⊕Y)') == 'xor[X, Y]'
+    assert translate_to_python('(X|Y)') == 'not[and[X, Y]]'
+    assert translate_to_python('(X~Y)') == 'not[xor[X, Y]]'
+    assert translate_to_python('(X↓Y)') == 'not[or[X, Y]]'
+    assert translate_to_python('(X→Y)') == 'or[not[X], Y]'
+    assert translate_to_python('(X∨Y)') == 'or[X, Y]'
+    assert translate_to_python('(X∧Y)') == 'and[X, Y]'
+    assert translate_to_python('(X̅)') == 'not[X]'
 
 
 if __name__ == '__main__':
-    print(translate_to_python('((X⊕Y)|(X⊕Y))'))
+    print(translate_to_python('(b̅→(((a̅⊕b)|(c̅↓a))∨(a~b)))'))
+
